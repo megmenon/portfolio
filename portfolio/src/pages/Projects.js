481068cfd8842895d5ead4f6ac3e6df43d6f2ca7
megmenon <mmenon.0419@gmail.com>
2018-12-06 22:47:47 -0800

    changes

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -18,7 +18,7 @@ class Projects extends Component{
                         <h4>
                         A full-stack ice-cream store application that has most functionalities of an e-commerce store. Functionalities include: pick an ice cream and cookie flavor, place an order, edit and delete existing order. 
                         Languages & technologies used:  <strong> HTML, CSS, JavaScript, Node.js, Express, Mongoose and MongoDB. </strong> <br/>
-                        <img src={require('../icecream.png')}/>
+                        
                         </h4>
                         <a href="https://icecream-sandwich-order-app.herokuapp.com/" target="_blank">
                         <button class="btn btn-danger" type="button">View</button></a>
@@ -32,7 +32,7 @@ class Projects extends Component{
                         <h4>
                         A full-stack web application with a React front-end. The app works like Twitter and is designed for soccer fans to banter about FIFA world cup.
                         Languages & technologies used: <strong> HTML, CSS, JavaScript, Mongoose, MongoDB, Node.js, Express and React. </strong><br/>
-                        <img src={require('../FeeFaa.png')}/>
+                        
                         </h4>
                         <a href="https://feefaa-frontend.herokuapp.com/" target="_blank">
                         <button class="btn btn-danger" type="button">View</button></a>
@@ -46,7 +46,7 @@ class Projects extends Component{
                         <h4>
                         A full-stack CRUD travel blog application built using Ruby on Rails. 
                         Languages & technologies used:  <strong> HTML, CSS, Ruby, Ruby on Rails. </strong> <br/>
-                        <img src={require('../travel.png')}/>
+                       
                         </h4>
                         <a href="https://github.com/megmenon/travel_on_rails" target="_blank">
                         <button class="btn btn-danger" type="button">View</button></a>
