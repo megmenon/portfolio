969b3767971dce74e7d91ccc6e430fdbee99f327
megmenon <mmenon.0419@gmail.com>
2018-12-06 22:10:59 -0800

    changed

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import '../index.css';
 import Mailto from 'react-protected-mailto';
-import { SocialIcon } from 'react-social-icons';
+// import { SocialIcon } from 'react-social-icons';
 
 class Footer extends Component{
 	render(){
@@ -12,10 +12,6 @@ class Footer extends Component{
     		<h3><strong>Megha Menon &copy; </strong></h3><br/><br/>
             <h4><Mailto className="mail" email='mmenon.0419@gmail.com'/></h4>
             <h4>510.936.3244</h4> <br/><br/>
-            <SocialIcon url="https://twitter.com/chefdeveloper" />
-            <SocialIcon url="https://www.linkedin.com/in/meghamenon" />
-            <SocialIcon url="https://github.com/megmenon" />
-            <SocialIcon url="https://angel.co/megha-menon" />
     		</div>
 			</footer>
 			</div>
