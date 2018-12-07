f20289ad7b21d806cc8f671535aa1a98c1620ed5
megmenon <mmenon.0419@gmail.com>
2018-12-06 22:43:47 -0800

    changes

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -12,6 +12,10 @@ class Footer extends Component{
     		<h3><strong>Megha Menon &copy; </strong></h3><br/><br/>
             <h4><Mailto className="mail" email='mmenon.0419@gmail.com'/></h4>
             <h4>510.936.3244</h4> <br/><br/>
+            <a href="https://twitter.com/chefdeveloper" target="_blank"><i className="fa fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/meghamenon/" target="_blank"><i className="fa fa-linkedin"></i></a>
+            <a href="https://angel.co/megha-menon" target="_blank"><i className="fa fa-angellist"></i></a>
+            <a href="https://github.com/megmenon" target="_blank"><i className="fa fa-github"></i></a>
     		</div>
 			</footer>
 			</div>
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -17,7 +17,8 @@ class Projects extends Component{
                         <h3 className="card-title des">Stagha Sandwiches</h3>
                         <h4>
                         A full-stack ice-cream store application that has most functionalities of an e-commerce store. Functionalities include: pick an ice cream and cookie flavor, place an order, edit and delete existing order. 
-                        Languages & technologies used:  <strong> HTML, CSS, JavaScript, Node.js, Express, Mongoose and MongoDB. </strong>
+                        Languages & technologies used:  <strong> HTML, CSS, JavaScript, Node.js, Express, Mongoose and MongoDB. </strong> <br/>
+                        <img src={require('../icecream.png')}/>
                         </h4>
                         <a href="https://icecream-sandwich-order-app.herokuapp.com/" target="_blank">
                         <button class="btn btn-danger" type="button">View</button></a>
@@ -30,7 +31,8 @@ class Projects extends Component{
                         <h3 className="card-title des">"Fee Faa"</h3>
                         <h4>
                         A full-stack web application with a React front-end. The app works like Twitter and is designed for soccer fans to banter about FIFA world cup.
-                        Languages & technologies used: <strong> HTML, CSS, JavaScript, Mongoose, MongoDB, Node.js, Express and React. </strong>
+                        Languages & technologies used: <strong> HTML, CSS, JavaScript, Mongoose, MongoDB, Node.js, Express and React. </strong><br/>
+                        <img src={require('../FeeFaa.png')}/>
                         </h4>
                         <a href="https://feefaa-frontend.herokuapp.com/" target="_blank">
                         <button class="btn btn-danger" type="button">View</button></a>
@@ -43,8 +45,8 @@ class Projects extends Component{
                         <h3 className="card-title des">Travel on Rails</h3>
                         <h4>
                         A full-stack CRUD travel blog application built using Ruby on Rails. 
-                        Languages & technologies used:  <strong> HTML, CSS, Ruby, Ruby on Rails. </strong></h4>
-                        <h4>
+                        Languages & technologies used:  <strong> HTML, CSS, Ruby, Ruby on Rails. </strong> <br/>
+                        <img src={require('../travel.png')}/>
                         </h4>
                         <a href="https://github.com/megmenon/travel_on_rails" target="_blank">
                         <button class="btn btn-danger" type="button">View</button></a>
